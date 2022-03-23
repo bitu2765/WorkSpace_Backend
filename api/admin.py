df97f25c895f6980b4b99da49ebcc95844077479
vraj0112 <vraj.s2001@gmail.com>
2022-03-23 20:04:44 +0530

    Changes in Admin APIs

--- api/admin.py
+++ api/admin.py
@@ -1,30 +1,47 @@
-from flask import Blueprint, make_response, render_template, request
+import json
+from flask import Blueprint,make_response,render_template,request, g
 from app import db
-from models import Admin, Location, Purchase_hist, Customer
+from models import Admin, Customer, Location, Subscription_plan, Plan_price, Purchase_hist
+from userauth import admin_auth
+from datetime import date
 
-admin = Blueprint('admin', __name__)
-
+admin = Blueprint('admin',__name__)
 
 @admin.route('/admin/profile', methods=['GET'])
+@admin_auth
 def admin_profile():
-    info = Admin.query.join(Location, Admin.tbl_location_id == Location.location_id).with_entities(Admin.admin_email,
-                                                                                                   Admin.admin_name,
-                                                                                                   Location.address,
-                                                                                                   Location.city,
-                                                                                                   Location.state).first()
-    return {
-        "admin": {
-            "email": info[0],
-            "name": info[1],
-            "address": info[2],
-            "city": info[3],
-            "state": info[4]
-        }
-    }
-
+    info = Admin.query.join(
+            Location, Admin.tbl_location_id == Location.location_id
+        ).with_entities(
+            Admin.admin_email, Admin.admin_name, Location.address, Location.city, Location.state
+        ).filter(Admin.admin_email == g.token).first()
+    
+    if bool(info):
+        resp = make_response({
+            "status": 200,
+            "admin": {
+                "email": info[0],
+                "name": info[1],
+                "address": info[2],
+                "city": info[3],
+                "state": info[4]
+            }
+        })
+        resp.headers['Access-Control-Allow-Credentials'] = 'true'
+        return resp
+    else:
+        resp = make_response(
+            {
+                "status_code": 404,
+                "message": "Admin doesn't exist."
+            }
+        )
+        resp.headers['Access-Control-Allow-Credentials'] = 'true'
+        return resp
 
 # all users details for admin side to show list of users
 @admin.route('/admin/user_details', methods=['GET'])
+@admin_auth
 def users_details():
     users = Customer.query.all()
     user_list = []
