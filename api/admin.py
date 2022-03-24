4aba7608c118bf3052ab88e9eda8d8e8147739ee
smitsonani77 <80664852+smitsonani77@users.noreply.github.com>
2022-03-24 18:37:30 +0530

    minor changes in admin.py file

--- api/admin.py
+++ api/admin.py
@@ -1,23 +1,25 @@
-from flask import Blueprint,make_response,render_template,request, g
+import json
+from flask import Blueprint, make_response, render_template, request, g
 from app import db
 from models import Admin, Customer, Location, Subscription_plan, Plan_price, Purchase_hist
 from userauth import admin_auth
 from datetime import date
 
-admin = Blueprint('admin',__name__)
+admin = Blueprint('admin', __name__)
+
 
 @admin.route('/admin/profile', methods=['GET'])
 @admin_auth
 def admin_profile():
     info = Admin.query.join(
-            Location, Admin.tbl_location_id == Location.location_id
-        ).with_entities(
-            Admin.admin_email, Admin.admin_name, Location.address, Location.city, Location.state
-        ).filter(Admin.admin_email == g.token).first()
-    
+        Location, Admin.tbl_location_id == Location.location_id
+    ).with_entities(
+        Admin.admin_email, Admin.admin_name, Location.address, Location.city, Location.state
+    ).filter(Admin.admin_email == g.token).first()
+
     if bool(info):
         resp = make_response({
-            "status_code": 200,
+            "status": 200,
             "admin": {
                 "email": info[0],
                 "name": info[1],
@@ -26,10 +28,44 @@ def admin_profile():
                 "state": info[4]
             }
         })
-        
-        
-        
+        resp.headers['Access-Control-Allow-Credentials'] = 'true'
+        return resp
+    else:
+        resp = make_response(
+            {
+                "status_code": 404,
+                "message": "Admin doesn't exist."
+            }
+        )
+        resp.headers['Access-Control-Allow-Credentials'] = 'true'
+        return resp
+
+
+# all users details for admin side to show list of users
+@admin.route('/admin/user_details', methods=['GET'])
+@admin_auth
+def users_details():
+    users = Customer.query.all()
+    user_list = []
+    for user in users:
+        _user = {
+            "name": user.name,
+            "email": user.email,
+            "email_verified": user.email_verify,
+            "is_block": user.block_user
+        }
+        user_list.append(_user)
+    resp = make_response({
+        "status_code": "200",
+        "customers": user_list
+    })
+    return resp
+
+
+# all user purchase plan history
+
 @admin.route('/admin/user_purchase_plan_details',methods = ["GET"])
+# @admin_auth
 def user_details():
     
     userInfo = db.session.query(Customer,Purchase_hist).filter(Customer.customer_id == Purchase_hist.tbl_customer_id).all()
@@ -75,24 +111,3 @@ def user_details():
         )
         resp.headers['Access-Control-Allow-Credentials'] = 'true'
         return resp
-
-# all users details for admin side to show list of users
-@admin.route('/admin/user_details', methods=['GET'])
-@admin_auth
-def users_details():
-    users = Customer.query.all()
-    user_list = []
-    for user in users:
-        _user = {
-            "name": user.name,
-            "email": user.email,
-            "email_verified": user.email_verify,
-            "is_block": user.block_user
-        }
-        user_list.append(_user)
-    resp = make_response({
-        "status_code": 200,
-        "customers": user_list
-    })
-    resp.headers['Access-Control-Allow-Credentials'] = 'true'
-    return resp
