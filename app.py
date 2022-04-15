bae0ff40272ae65e8cb873bb7bf839992f56d4cb
BHAUTIK SUDANI <68993493+bitu2765@users.noreply.github.com>
2022-04-16 00:46:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,8 +17,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://' + user + ':' + passwo
 
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = 'veetmoradiya7823@gmail.com'
-app.config['MAIL_PASSWORD'] = 'veet@1234'
+app.config['MAIL_USERNAME'] = ''
+app.config['MAIL_PASSWORD'] = ''
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
